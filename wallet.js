21495f73eca1149839796f6024bbc8f96b5245f1
UnuSec <unusec@yahoo.com>
2018-01-07 02:50:30 +0200

    inital

--- wallet.js
+++ wallet.js
@@ -1,7 +1,8 @@
 /*	
 	A simple cold storage wallet generator, using the official API made available by Ripple. 
 	This code can be run offline
-*/	
+*/
+
 const RippleAPI = require('ripple-lib').RippleAPI;
 const api = new RippleAPI();
 const wallet = api.generateAddress();
